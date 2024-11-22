468f9df1835462371e532a8437017722d155bab7
John Ndelembi <williamjohnie61@gmail.com>
2024-11-22 10:24:17 +0300

    Update settings.py

--- youtube_downloader/settings.py
+++ youtube_downloader/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".onrender.com", "localhost", ".deploy.tz", "127.0.0.1"]
+ALLOWED_HOSTS = [".onrender.com", "localhost", "freebie.deploy.tz", "127.0.0.1"]
 
 
 # Application definition
